47c169274f32d1f452afc5ac257a23ea93834d63
manishaac2002 <manishaac2002@gmail.com>
2024-11-06 16:53:37 +0530

    feat : tech component is working on.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
       <Company />
       <DevelopAi />
       <DeployAi />
+      <TechnologyAi />
+      <DeployAi />
+      <Header />
 
     </>
   )
@@ -208,10 +211,41 @@ function DeployAi() {
   )
 }
 
-{/* start here------------------------------------------------------------------------------------- */ }
-// function TechnologyAi () {
-// }
+function TechnologyAi() {
+  return (
+    <div className="mx-44 pt-40  ">
+      <div className="border border-gray-300  bg-indigo-50 rounded-3xl pt-16">
+        <div className="py-4 flex justify-center">
+          <a href="" className="border border-indigo-400 bg-slate-100 px-6 py-2 rounded-3xl flex justify-center shadow-indigo-200 text-lg shadow-lg">
+            <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">!Font Awesome Free 6.6.0 by
+              @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons,
+              Inc.--<path d="M320 0c17.7 0 32 14.3 32 32l0 64 120 0c39.8 0 72 32.2 72 72l0 272c0 39.8-32.2 72-72 72l-304 0c-39.8 0-72-32.2-72-72l0-272c0-39.8 32.2-72 72-72l120 0 0-64c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zM264 256a40 40 0 1 0 -80 0 40 40 0 1 0 80 0zm152 40a40 40 0 1 0 0-80 40 40 0 1 0 0 80zM48 224l16 0 0 192-16 0c-26.5 0-48-21.5-48-48l0-96c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-16 0 0-192 16 0z" />
+            </svg> <span className="pl-2 ">TECHNOLOGY</span>
+          </a>
+        </div>
 
+        <div className="flex justify-center text-gray-900">
+          <p className="text-6xl font-light py-4">Transforming
+            <span className="font-medium "> Experiences <br />with Powerful </span>
+            Features</p>
+        </div>
+
+        <p className="text-xl text-gray-500 pt-4 flex justify-center">Create AI Agents from scratch powered by any combination</p>
+        
+        <div className="grid grid-cols-2 m-10 px-32">
+          <div className="border border-gray-300">
+             <img className="pt-5" src="/person.png" alt="" />
+            test</div>
+          <div className="border border-gray-300 pl-10">
+             <img className="pt-5" src="/ai.png" alt="" />
+            test</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+{/* start here------------------------------------------------------------------------------------- */ }
 // function PlatformAi () {
 
 // }
